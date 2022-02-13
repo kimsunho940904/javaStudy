66c1117fbc92c36db20e9fd2a4e6260aec282eed
Kim, Sunho <63573287+kimsunho940904@users.noreply.github.com>
2022-02-13 16:06:57 +0900

    Apring Start

---
+++ Apring/src/main/java/com/apring/ApringApplication.java
@@ -0,0 +1,13 @@
+package com.apring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApringApplication.class, args);
+    }
+
+}
