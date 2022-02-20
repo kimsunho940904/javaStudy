dcf854a9f3471ddea0395d5e52bd4c4f42e98045
Kim, Sunho <63573287+kimsunho940904@users.noreply.github.com>
2022-02-20 13:59:41 +0900

    김선호 게시판 작성중

--- Apring/src/main/java/com/apring/mapper/BoardMapper.java
+++ Apring/src/main/java/com/apring/mapper/BoardMapper.java
@@ -8,7 +8,28 @@ import java.util.List;
 // @Repository = MapperScan 대상
 @Repository
 public interface BoardMapper {
-    
-    List<Board> list();
+
+    /**
+     * @작성자 : 김선호
+     * @작성일 : 2022.02.20
+     */
+    // 게시글 개수
+    public int boardCount() throws Exception;
+
+    // 게시글 목록
+    public List<Board> boardList() throws Exception;
+
+    // 게시글 상세
+    public Board boardDetail(Long bno) throws Exception;
+
+    // 게시글 작성
+    public Long boardInsert(Board board) throws Exception;
+
+    // 게시글 수정
+    public Long boardUpdate(Board board) throws Exception;
+
+    // 게시글 삭제
+    public Long boardDelete(Long bno) throws Exception;
+
 
 }
