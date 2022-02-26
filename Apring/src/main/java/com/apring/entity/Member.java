a0c39cecf76a2dfa1d26e533dc1e703303f1cf08
chan <pchyeok@naver.com>
2022-02-26 11:58:01 +0900

    test commit

---
+++ Apring/src/main/java/com/apring/entity/Member.java
@@ -0,0 +1,15 @@
+package com.apring.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class Member {
+
+    // 22.02.26 박찬혁
+	
+    
+}// end Member
