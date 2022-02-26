8c1a695cbf9c9df5d0bcb37333d5fd38393528bb
Kim, Sunho <63573287+kimsunho940904@users.noreply.github.com>
2022-02-26 23:35:39 +0900

    css 수정 및 고객센터 제작중

--- Apring/src/main/java/com/apring/controller/MemberController.java
+++ Apring/src/main/java/com/apring/controller/MemberController.java
@@ -6,9 +6,4 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MemberController {
 
-    @GetMapping("/")
-    public String main() {
-        return "/main";
-    }
-
 }// end MemberController
