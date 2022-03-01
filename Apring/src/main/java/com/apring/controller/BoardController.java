546c8e6a2a7f542743b8bb06dc5003ea5e6868ad
Kim, Sunho <63573287+kimsunho940904@users.noreply.github.com>
2022-03-01 19:42:50 +0900

    글 작성 기능 구현

--- Apring/src/main/java/com/apring/controller/BoardController.java
+++ Apring/src/main/java/com/apring/controller/BoardController.java
@@ -1,11 +1,13 @@
 package com.apring.controller;
 
+import com.apring.entity.Board;
 import com.apring.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -13,7 +15,7 @@ public class BoardController {
 
     /**
      * @작성자 : 김선호
-     * @작성일 : 2022.02.20
+     * @작성일 : 2022.03.01
      */
 
     @Autowired
@@ -23,6 +25,18 @@ public class BoardController {
     public String boardList(Model model) throws Exception {
         model.addAttribute("boardList", boardService.boardList());
         return "/board/list";
+    }
+
+    @GetMapping("/board/insertView")
+    public String boardInsertView() throws Exception {
+        return "/board/insert";
+    }
+
+    @PostMapping("/board/insert")
+    public String boardInsert(Board board) throws Exception {
+        boardService.boardInsert(board);
+
+        return "redirect:/board/list";
     }
 
 
